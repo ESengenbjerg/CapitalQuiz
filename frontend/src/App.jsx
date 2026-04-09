72e2643934e5fa0bffdf4aa896c46479ec2795d5
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-04-09 12:01:50 +0200

    Added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,24 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
+import StartPage from "./pages/startpage";
+import GamePage from "./pages/gamepage";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   // const [count, setCount] = useState(0);
 
-  return <>{/* Router */}</>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/pages/startpage.jsx
+++ frontend/src/pages/startpage.jsx
@@ -1,6 +1,6 @@
 import styles from "./startpage.module.css";
 
-function App() {
+function StartPage() {
   // const [count, setCount] = useState(0);
 
   return (
@@ -14,4 +14,4 @@ function App() {
   );
 }
 
-export default App;
+export default StartPage;
