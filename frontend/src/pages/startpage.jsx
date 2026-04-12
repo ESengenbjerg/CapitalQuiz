7fc22c7100e797d432024ef5382037b527b28f58
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-04-12 20:43:34 +0200

    Added highscore list

--- frontend/src/pages/startpage.jsx
+++ frontend/src/pages/startpage.jsx
@@ -1,9 +1,16 @@
 import styles from "./startpage.module.css";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function StartPage() {
-  // const [count, setCount] = useState(0);
+  const [scores, setScores] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:5026/highscores")
+      .then((res) => res.json())
+      .then((data) => setScores(data));
+  }, []);
 
   function handleNewGame() {
     navigate("/game");
@@ -16,6 +23,17 @@ function StartPage() {
 
         {/* This page only needs to redirect to gamepage. Backend starts the actual game logic */}
         <button onClick={handleNewGame}>New game</button>
+
+        <section>
+          <h2>Highscores</h2>
+          <ol>
+            {scores.map((score, i) => (
+              <li key={i}>
+                {score.name}: {score.score}
+              </li>
+            ))}
+          </ol>
+        </section>
       </main>
     </>
   );
