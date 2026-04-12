fee15c2921848de30fe7c29c2d709794cdcb22df
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-04-12 20:03:39 +0200

    Added redirect to game page

--- frontend/src/pages/startpage.jsx
+++ frontend/src/pages/startpage.jsx
@@ -1,14 +1,21 @@
 import styles from "./startpage.module.css";
+import { useNavigate } from "react-router-dom";
 
 function StartPage() {
   // const [count, setCount] = useState(0);
+  const navigate = useNavigate();
+
+  function handleNewGame() {
+    navigate("/game");
+  }
 
   return (
     <>
       <main>
         <h1>Welcome to the Capital Quiz!</h1>
 
-        <button>New game</button>
+        {/* This page only needs to redirect to gamepage. Backend starts the actual game logic */}
+        <button onClick={handleNewGame}>New game</button>
       </main>
     </>
   );
